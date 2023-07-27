e9116fea7d03e08b0d353dfa4cca0eb6159f7848
hector-baez <hbaez@oxy.edu>
2023-07-26 23:48:47 -0500

    chatbot testing

--- chatbot/chatbot_presentation/src/main/java/com/example/chatbot_presentation/chat_overview/ChatScreen.kt
+++ chatbot/chatbot_presentation/src/main/java/com/example/chatbot_presentation/chat_overview/ChatScreen.kt
@@ -31,60 +31,53 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.annotation.ExperimentalCoilApi
 import com.example.chatbot_presentation.chat_overview.model.Message
+import com.hbaez.core.R
 import com.hbaez.core_ui.LocalSpacing
 import com.hexascribe.chatbotbuilder.ChatBot
 import com.hexascribe.chatbotbuilder.base.RoleEnum
 
 @Composable
-fun ChatScreen() {
-    val chatBot = ChatBot.Builder("PLACEHOLDER")
-        .setDarkMode(true)
-        .addMessage(RoleEnum.ASSISTANT, "Hi, how can I help you today?")
-        .addPreSeededMessage(RoleEnum.SYSTEM, "You are a helpful seller car assistant")
-        .setInputFieldBorderWidth(1)
-        .build()
-    chatBot.ComposeScreen()
+fun ChatScreen(
+    viewModel: ChatViewModel = hiltViewModel()
+){
+    val spacing = LocalSpacing.current
+    val state = viewModel.state
+    Scaffold(
+        topBar = { AppBar(title = "Chatbot") },
+        content = {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(bottom = spacing.spaceExtraLarge + spacing.spaceSmall)
+            ) {
+                val chatBot = ChatBot.Builder("PLACEHOLDER")
+                    .setDarkMode(true)
+                    .addMessage(RoleEnum.ASSISTANT, "Hi, how can I help you today?")
+                    .addPreSeededMessage(RoleEnum.SYSTEM, "You are a helpful personal trainer")
+                    .addPreSeededMessage(RoleEnum.SYSTEM, String.format(stringResource(id = R.string.chatgpt_preseed_msg), 23, "male", "beginner", 69, 190, "low", "lose", "upper body"))
+                    .setInputFieldBorderWidth(1)
+                    .build()
+                chatBot.ComposeScreen()
+            }
+        }
+    )
 }
-//@Composable
-//fun ChatScreen(
-//    viewModel: ChatViewModel = hiltViewModel()
-//){
-//    val spacing = LocalSpacing.current
-//    val state = viewModel.state
-//    Scaffold(
-//        topBar = { AppBar(title = "Chatbot") },
-//        content = {
-//            Column(
-//                modifier = Modifier
-//                    .fillMaxSize()
-//                    .padding(bottom = spacing.spaceExtraLarge + spacing.spaceSmall)
-//            ) {
-//                MessageList(
-//                    modifier = Modifier.weight(1f),
-//                    messages = state.messages
-//                )
-//                UserInputField(
-//                    modifier = Modifier.padding(top = 16.dp)
-//                )
-//            }
-//        }
-//    )
-//}
 //
-//@Composable
-//fun AppBar(title: String) {
-//    TopAppBar(
-//        title = { Text(text = title) },
-//        backgroundColor = MaterialTheme.colors.primary,
-//        contentColor = MaterialTheme.colors.onSecondary
-//    )
-//}
+@Composable
+fun AppBar(title: String) {
+    TopAppBar(
+        title = { Text(text = title) },
+        backgroundColor = MaterialTheme.colors.primary,
+        contentColor = MaterialTheme.colors.onSecondary
+    )
+}
 //
 //@Composable
 //fun MessageList(
