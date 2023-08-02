7344a8f6a3afd79354f7c6e24f6239afd25b05e2
hector-baez <hbaez@oxy.edu>
2023-08-02 02:28:38 -0500

    rm testing dep

--- wearable/build.gradle.kts
+++ wearable/build.gradle.kts
@@ -106,4 +106,25 @@ dependencies {
 
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.17.0")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.0-alpha03")
+
+    testImplementation(Testing.junit4)
+    testImplementation(Testing.junitAndroidExt)
+    testImplementation(Testing.truth)
+    testImplementation(Testing.coroutines)
+    testImplementation(Testing.turbine)
+    testImplementation(Testing.composeUiTest)
+    testImplementation(Testing.mockk)
+    testImplementation(Testing.mockWebServer)
+
+//    androidTestImplementation(Testing.junit4)
+//    androidTestImplementation(Testing.junitAndroidExt)
+//    androidTestImplementation(Testing.truth)
+//    androidTestImplementation(Testing.coroutines)
+//    androidTestImplementation(Testing.turbine)
+//    androidTestImplementation(Testing.composeUiTest)
+//    androidTestImplementation(Testing.mockkAndroid)
+//    androidTestImplementation(Testing.mockWebServer)
+//    androidTestImplementation(Testing.hiltTesting)
+//    kaptAndroidTest(DaggerHilt.hiltCompiler)
+//    androidTestImplementation(Testing.testRunner)
 }
