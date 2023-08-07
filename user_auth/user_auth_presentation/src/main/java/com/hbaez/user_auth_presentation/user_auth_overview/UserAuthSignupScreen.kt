446edabf2632da59e75c09c3155ed0d486df274f
baezfb <baezdevs@gmail.com>
2023-08-07 14:42:07 -0500

    signup screen view ui Alpha 1.0.0

--- user_auth/user_auth_presentation/src/main/java/com/hbaez/user_auth_presentation/user_auth_overview/UserAuthSignupScreen.kt
+++ user_auth/user_auth_presentation/src/main/java/com/hbaez/user_auth_presentation/user_auth_overview/UserAuthSignupScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -27,11 +28,10 @@ import coil.annotation.ExperimentalCoilApi
 import com.hbaez.core.R
 import com.hbaez.core.util.UiEvent
 import com.hbaez.core_ui.LocalSpacing
-import com.hbaez.user_auth_presentation.components.BasicButton
 import com.hbaez.user_auth_presentation.components.BasicTextButton
+import com.hbaez.user_auth_presentation.components.Button
 import com.hbaez.user_auth_presentation.components.EmailField
 import com.hbaez.user_auth_presentation.components.PasswordField
-import kotlinx.coroutines.flow.collect
 
 @ExperimentalCoilApi
 @Composable
@@ -39,7 +39,7 @@ fun UserAuthSignupScreen(
     scaffoldState: ScaffoldState,
     openAndPopUp: (String, String) -> Unit,
     viewModel: UserAuthViewModel = hiltViewModel()
-){
+) {
     val spacing = LocalSpacing.current
     val state = viewModel.state
     val context = LocalContext.current
@@ -51,6 +51,7 @@ fun UserAuthSignupScreen(
                         message = event.message.asString(context)
                     )
                 }
+
                 else -> Unit
             }
         }
@@ -59,46 +60,62 @@ fun UserAuthSignupScreen(
     Column(
         Modifier
             .fillMaxSize()
+            .background(MaterialTheme.colors.primary)
     ) {
+        Spacer(modifier = Modifier.height(spacing.spaceSmall))
+        //TODO: add back button
+        Text(
+            stringResource(id = R.string.sign_up),
+            style = MaterialTheme.typography.h2,
+            modifier = Modifier
+                .padding(16.dp, 4.dp)
+        )
+
         Spacer(modifier = Modifier.height(spacing.spaceExtraExtraLarge))
-        Spacer(modifier = Modifier.height(spacing.spaceExtraLarge))
+
         Column(
             modifier = Modifier
                 .fillMaxWidth()
                 .weight(1f)
                 .clip(
                     RoundedCornerShape(
-                        topStart = 50.dp,
-                        topEnd = 50.dp
+                        topStart = 15.dp,
+                        topEnd = 15.dp
                     )
                 )
-                .background(MaterialTheme.colors.primary)
+                .background(Color.White)
                 .padding(
                     horizontal = spacing.spaceSmall
                 )
-        ){
-            Spacer(modifier = Modifier.height(spacing.spaceExtraLarge))
-            Text(
-                stringResource(id = R.string.sign_up),
-                textAlign = TextAlign.Center,
-                style = MaterialTheme.typography.h2,
-                modifier = Modifier
-                    .padding(16.dp, 4.dp)
-                    .align(Alignment.CenterHorizontally)
-            )
-            Spacer(modifier = Modifier.height(spacing.spaceLarge))
-            EmailField(
-                value = state.email,
-                onNewValue = {
-                    viewModel.onEvent(UserAuthEvent.OnEmailFieldChange(it))
-                },
+        ) {
+            Spacer(modifier = Modifier.height(spacing.spaceMedium))
+            Column(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(16.dp, 4.dp)
-            )
-            Column(
-                Modifier.fillMaxWidth()
+                    .padding(horizontal = spacing.spaceSmall),
             ) {
+                Text(
+                    stringResource(id = R.string.sign_up_header),
+                    color = MaterialTheme.colors.primary,
+                    style = MaterialTheme.typography.h2,
+                )
+                Text(
+                    stringResource(id = R.string.sign_up_subheader),
+                    color = Color.Gray,
+                    style = MaterialTheme.typography.h4,
+                )
+
+                Spacer(modifier = Modifier.height(spacing.spaceLarge))
+
+                EmailField(
+                    value = state.email,
+                    onNewValue = {
+                        viewModel.onEvent(UserAuthEvent.OnEmailFieldChange(it))
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp, 4.dp)
+                )
                 PasswordField(
                     value = state.password,
                     onNewValue = {
@@ -119,18 +136,42 @@ fun UserAuthSignupScreen(
                         .padding(16.dp, 4.dp),
                     placeholder = R.string.retype_password
                 )
-            }
-            Spacer(modifier = Modifier.height(spacing.spaceMedium))
-            Column(
-                Modifier.fillMaxWidth()
-            ) {
-                BasicButton(
+
+                Spacer(modifier = Modifier.height(spacing.spaceMedium))
+
+                Button(
                     R.string.sign_up,
                     Modifier
                         .fillMaxWidth()
                         .padding(16.dp, 8.dp)
                 ) {
-                    viewModel.onEvent(UserAuthEvent.OnSignupClick(state.email, state.password, state.passwordRetyped, openAndPopUp))
+                    viewModel.onEvent(
+                        UserAuthEvent.OnSignupClick(
+                            state.email,
+                            state.password,
+                            state.passwordRetyped,
+                            openAndPopUp
+                        )
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(spacing.spaceLarge))
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = "Already have an account?",
+                        color = Color.Gray,
+                        textAlign = TextAlign.Center,
+                    )
+                    
+                    BasicTextButton(text = R.string.sign_in, modifier = Modifier.padding(start = spacing.spaceSmall)) {
+                        //TODO: navigate to sign in screen
+                    }
                 }
             }
         }
